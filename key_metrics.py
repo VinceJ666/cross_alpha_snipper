fb1c86fbeb41f828100a31a3394689486d5d2152
VinceJ666 <vinceji666@gmail.com>
2023-06-12 23:44:10 +0800

    spilt the functionality into metric moudule and visualization moudule

---
+++ key_metrics.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 20:24:24 2022
+
+@author: vince
+"""
+
+import numpy as np
+from scipy import stats
+
+def annual_return(ret,trading_days=365):
+    s=(ret+1).cumprod()      
+    total_ret=s[-1]-1
+    num_years=(ret.index.max()-ret.index.min()).days/trading_days
+    annual_return = (1. + total_ret) ** (1. / num_years) - 1
+    return annual_return
+
+def sharpe_ratio(ret,trading_days=365):
+    if np.std(ret)!=0:
+        return np.mean(ret)/np.std(ret)*np.sqrt(trading_days)  
+    else:
+        return np.mean(ret)*np.sqrt(trading_days)  
+
+def calmar_ratio(ret):
+    max_dd = max_drawdown(ret)
+    if max_dd < 0:
+        calmar = annual_return(ret) / abs(max_dd)
+        if np.isinf(calmar):
+            return np.nan
+        else:
+            return calmar
+    else:
+        return np.nan
+
+def win_rate(ret,benchmark=0):
+    win=((ret-benchmark)>0).sum()
+    loss=((ret-benchmark)<0).sum()
+    return win/(win+loss)
+    
+def drawdown_series(ret):
+    s=(ret+1).cumprod()
+    cummax=s.cummax()
+    return s/cummax-1
+
+def max_drawdown(ret):
+    s=(ret+1).cumprod()
+    cummax=s.cummax()
+    return (s/cummax-1).min()
+    
+def ranked_ic_series(df0,f1,f2):
+    df=df0[[f1,f2]].dropna().sort_index()
+    df[f1]=df.groupby('datetime')[f1].rank()
+    df[f2]=df.groupby('datetime')[f2].rank()
+    df=df.groupby('datetime').apply(lambda x:x[f1].corr(x[f2]))
+    return df.dropna()
+
+def ranked_ic_stats(df):
+    tStat,pValue = stats.ttest_1samp(df,0)
+    ic_mean=df.mean()
+    icir=df.mean()/df.std()
+    return ic_mean, icir, tStat, pValue
+
+def turnover(l_candid,s_candid,l_cur,s_cur):
+    total=len(l_cur)+len(s_cur)
+    close_position=0
+    for i in l_cur:
+        if i not in l_candid:
+            close_position += 1
+    for i in s_cur:
+        if i not in s_candid:
+            close_position += 1
+    open_position=0
+    for i in l_candid:
+        if i not in l_cur:
+            open_position += 1
+    for i in s_candid:
+        if i not in s_cur:
+            open_position += 1
+    return 0.5*(close_position+open_position)/total
+
+
+    
+    
+    
+    
+    
+    
--- metrics_visualization.py
+++ metrics_visualization.py
@@ -8,66 +8,14 @@ Created on Sat Aug  6 20:24:24 2022
 
 import pandas as pd
 import numpy as np
-from scipy import stats
-#import empyrical
+
 from bokeh.layouts import column
 from bokeh.models import BoxAnnotation, Paragraph, ColumnDataSource ,DataTable, TableColumn, DateFormatter, HoverTool
 from bokeh.plotting import figure, output_file, show
 
+from key_metrics import annual_return, sharpe_ratio, calmar_ratio, win_rate
+from key_metrics import drawdown_series, max_drawdown, ranked_ic_series, ranked_ic_stats, turnover
 
-def icir(df0,f1,f2,plot=True):
-    df=df0[[f1,f2]].dropna().sort_index()
-    df[f1]=df.groupby('datetime')[f1].rank()
-    df[f2]=df.groupby('datetime')[f2].rank()
-    df=df.groupby('datetime').apply(lambda x:x[f1].corr(x[f2]))
-    df=df.dropna()
-    tStat,pValue = stats.ttest_1samp(df,0)
-    tools=['pan','wheel_zoom','box_zoom','reset']
-    ic_mean=df.mean()
-    icir=df.mean()/df.std()
-    p=figure(width=800, height=300, x_axis_type='datetime', tools=tools, title=f"IC Mean:{round(ic_mean,4)}, ICIR:{round(icir,4)}, P-Value:{round(pValue,4)}, T-Statistic:{round(tStat,4)}, Positive%:{round((df>0).mean()*100,2)}%, Negative%:{round((df<0).mean()*100,2)}%")
-    p.line(df.index,df.values,color='navy') 
-    hover=HoverTool(tooltips=[('Date','@x{%Y-%m-%d %H:%M:%S}'), ('IC','@y')],formatters={'@x':'datetime'})
-    p.add_tools(hover)
-    if plot:
-        show(p)
-    return p
-
-def max_drawdown(ret,compound):
-    if compound:
-        s=(ret+1).cumprod()
-    else:
-        s=ret.cumsum()+1
-    cummax=s.cummax()
-    return (s/cummax-1).min()
-
-def annual_return(ret,compound):
-    if compound:
-        s=(ret+1).cumprod()      
-    else:
-        s=ret.cumsum()+1
-    total_ret=s[-1]-1
-    num_years=(ret.index.max()-ret.index.min()).days/365
-    annual_return = (1. + total_ret) ** (1. / num_years) - 1
-    return annual_return
-
-def sharpe_ratio(ret):
-    if np.std(ret)!=0:
-        return np.mean(ret)/np.std(ret)*np.sqrt(365)  
-    else:
-        return np.mean(ret)*np.sqrt(365)  
-
-def calmar_ratio(ret,compound):
-    max_dd = max_drawdown(ret,compound)
-    if max_dd < 0:
-        calmar = annual_return(ret,compound) / abs(max_dd)
-    else:
-        return np.nan
-    
-    if np.isinf(calmar):
-        return np.nan
-
-    return calmar
 
 
 def ranked_hedge_analysis(df0,f,r,n=5,fees=0,leverage=1,slippage=0,compound=True,plot=True,weight=None):
@@ -114,7 +62,7 @@ def ranked_hedge_analysis(df0,f,r,n=5,fees=0,leverage=1,slippage=0,compound=True
 
 def quick_analysis(df0,f,r,n=5,fees=0,leverage=1,slippage=0,compound=True,weight=None,info=''):
     p0=Paragraph(text=f"""{f} {df0.index.get_level_values('datetime').min().date()} to {df0.index.get_level_values('datetime').max().date()};\n fees:{fees}, leverge:{leverage}, slippage:{slippage}"""+info,width=800)
-    p1=icir(df0,f,r,plot=False)
+    p1=ranked_ic_series(df0,f,r,plot=False)
     p2,ret=ranked_hedge_analysis(df0,f,r,n,fees,leverage,slippage,compound,plot=False,weight=weight)
     show(column(p0,p1,p2))
     
